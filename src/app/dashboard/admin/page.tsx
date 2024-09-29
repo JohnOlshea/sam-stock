7a33cef44ffc473badc1763c7df83df176e3f719
John <johnolshea@gmail.com>
2024-09-30 00:06:22 +0100

    Fix eslint description not valid export error

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -68,8 +68,8 @@ import {
 import { FaArrowUpRightDots } from "react-icons/fa6";
 import { FaDollarSign } from "react-icons/fa";
 
-export const description =
-  "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
+// export const description =
+//   "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action."
 
 export default function Dashboard() {
   return (
