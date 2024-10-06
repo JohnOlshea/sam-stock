69bb798be3fbb35e173210303873f947406b8ec1
John <johnolshea@gmail.com>
2024-10-06 21:41:54 +0100

    Add withdraw link for mobile view

--- src/app/dashboard/user/layout.tsx
+++ src/app/dashboard/user/layout.tsx
@@ -122,6 +122,13 @@ export default function DashboardLayout({
                     <Users className="h-5 w-5" />
                     Deposit Cash
                   </Link>
+                  <Link
+                    href="#"
+                    className="mx-[-0.65rem] flex items-center gap-4 px-3 py-2 text-muted-foreground hover:text-foreground"
+                  >
+                    <Users className="h-5 w-5" />
+                    Withdraw Cash
+                  </Link>
                 </nav>
               </SheetContent>
             </Sheet>
